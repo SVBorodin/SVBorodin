25caa59cbf07186a1c59153e9e4529c89b366cd5
SVBorodin <borodin_sergei_2007@mail.ru>
2025-09-24 22:54:01 +0200

    1

---
+++ .vscode/Laba/121.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <limits>
+int main()
+{
+    int h ;
+    int osn1 ;
+    int osn2 ;
+    std::cout << "Введите высоту" << '\n';
+    std::cin >> h;
+    while 1 <= h <= 255
+    std::cout << "Введите основание номер 1" << '\n';
+    std::cin >> osn1;
+    std::cout << "Введите основание номер 2" << '\n';
+    std::cin >> osn2;
+}
