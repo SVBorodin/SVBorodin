4a3b7e04d0266451e8894e71151b8e8021b79847
SVBorodin <borodin_sergei_2007@mail.ru>
2025-09-26 14:57:47 +0200

    1

--- .vscode/Laba/121.cpp
+++ .vscode/Laba/121.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <limits>
+#include <cstring>
+#include <cmath>
 int main()
 {
-    int h ;
-    int osn1 ;
-    int osn2 ;
-    std::cout << "Введите высоту" << '\n';
+    short h ; //Вмещает от -2^15 до 2^15, что подходит по размеру
+    short osn1 ; //Та же ситуация с 2 основаниями
+    short osn2 ; 
+    int S; //По идее должно вмешать до 255тыс
+    std::cout << "Введите высоту (|x| ≤ 255):" << '\n';
     std::cin >> h;
-    while 1 <= h <= 255
-    std::cout << "Введите основание номер 1" << '\n';
+    std::cout << "Введите основание номер 1 (|x| ≤ 1000):" << '\n';
     std::cin >> osn1;
-    std::cout << "Введите основание номер 2" << '\n';
+    std::cout << "Введите основание номер 2 (|x| ≤ 1000):" << '\n';
     std::cin >> osn2;
+    S = (osn1 + osn2)*h / 2;
+    std::cout << S ; 
 }
